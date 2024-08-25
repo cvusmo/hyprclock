f0ab9d1090bc89b31b21ddd5ce5cca53b75023f3
cvusmo <blacksheepcosmo@gmail.com>
2024-08-25 15:17:23 -0500

    fully functional widget

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,29 @@
-use std::cell::Cell;
-use std::rc::Rc;
-
 use gio::Settings;
+use chrono::Local;
 
-use glib::clone;
 use gtk4 as gtk;
 use gtk::prelude::*;
-use gtk::{gio, glib, Align, Application, ApplicationWindow, Button, Orientation, Switch};
+use gtk::{gio, glib, Align, Application, ApplicationWindow, Label, Orientation, Switch};
 
 const APP_ID: &str = "org.cvusmo.Hyprclock";
 
 fn main() -> glib::ExitCode {
-
     let app = Application::builder().application_id(APP_ID).build();
-
     app.connect_activate(build_ui);
-
     app.run()
 }
 
 fn build_ui(app: &Application) {
-
     let settings = Settings::new(APP_ID);
-
     let is_switch_enabled = settings.boolean("is-switch-enabled");
 
+//    let time = Local::now().format("%H:%M:%S").to_string();
+
     let switch = Switch::builder()
-        .margin_top(48)
-        .margin_bottom(48)
-        .margin_start(48)
-        .margin_end(48)
-        .valign(Align::Center)
-        .halign(Align::Center)
+        .margin_top(3)
+        .margin_bottom(3)
+        .valign(Align::End)
+        .halign(Align::Start)
         .state(is_switch_enabled)
         .build();
     
@@ -39,52 +31,19 @@ fn build_ui(app: &Application) {
         settings
             .set_boolean("is-switch-enabled", is_enabled)
             .expect("Could not set setting.");
-        
         glib::Propagation::Proceed
     });
 
-    let button_increase = Button::builder()
-        .label("Increase")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
+    let clock_label = Label::builder()
+        .label(&get_current_time())
+        .halign(Align::Center)
+        .valign(Align::Start)
         .build();
 
-    let button_decrease = Button::builder()
-        .label("Decrease")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
-
-    let number = Rc::new(Cell::new(0));
-
-    button_increase.connect_clicked(clone!(
-        #[weak]
-        number,
-        #[weak]
-        button_decrease,
-        move |_| {
-            number.set(number.get() + 1);
-            button_decrease.set_label(&number.get().to_string());
-        }
-    ));
-    button_decrease.connect_clicked(clone!(
-        #[weak]
-        button_increase,
-        move |_| {
-            number.set(number.get() - 1);
-            button_increase.set_label(&number.get().to_string());
-        }
-    ));
-    
     let gtk_box = gtk::Box::builder()
         .orientation(Orientation::Vertical)
         .build();
-    gtk_box.append(&button_increase);
-    gtk_box.append(&button_decrease);
+    gtk_box.append(&clock_label);
     gtk_box.append(&switch);
 
     let window = ApplicationWindow::builder()
@@ -94,4 +53,9 @@ fn build_ui(app: &Application) {
         .build();
 
     window.present();
+}
+
+fn get_current_time() -> String {
+    let current_time = Local::now();
+    current_time.format("%H:%M:%S").to_string() 
 }
